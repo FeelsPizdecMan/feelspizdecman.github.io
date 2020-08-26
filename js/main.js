d0dfacf11b82a0b111dbeb91e27b733c457bd7c8
studiobarbershop <niznom-vizdo2-Riprym>
2020-08-26 20:31:39 +0500

    dd

--- js/main.js
+++ js/main.js
@@ -95,3 +95,21 @@ var display = function(block_name, title) {
     //... and adds the "active" class to the current step:
     x[n].className += " active";
   }
+
+  (function() {
+    function scrollHorizontally(e) {
+        e = window.event || e;
+        var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+        document.getElementById('infra').scrollLeft -= (delta * 40); // Multiplied by 40
+        e.preventDefault();
+    }
+    if (document.getElementById('infra').addEventListener) {
+        // IE9, Chrome, Safari, Opera
+        document.getElementById('infra').addEventListener('mousewheel', scrollHorizontally, false);
+        // Firefox
+        document.getElementById('infra').addEventListener('DOMMouseScroll', scrollHorizontally, false);
+    } else {
+        // IE 6/7/8
+        document.getElementById('infra').attachEvent('onmousewheel', scrollHorizontally);
+    }
+})();
