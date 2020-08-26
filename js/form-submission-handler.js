507130a09b34a87f931095151a923a92ddfa531d
studiobarbershop <niznom-vizdo2-Riprym>
2020-08-26 18:45:03 +0500

    ddd

--- js/form-submission-handler.js
+++ js/form-submission-handler.js
@@ -77,6 +77,7 @@
           var thankYouMessage = form.querySelector(".thankyou_message");
           if (thankYouMessage) {
             thankYouMessage.style.display = "block";
+            location.href = "thanks.html";
           }
         }
     };
