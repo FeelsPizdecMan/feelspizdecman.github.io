ff357b00eb9dc11d5101b1d7ffaea34e9b19d10d
yan <58911833+FeelsPizdecMan@users.noreply.github.com>
2020-12-11 16:09:29 +0500

    grads

---
+++ scripts/main.js
@@ -0,0 +1,62 @@
+function myFunction() {
+    var x = document.getElementById("more-about");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function myFunction2() {
+    var x = document.getElementById("more-about2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function myFunction3() {
+    var x = document.getElementById("more-about3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function myFunction4() {
+    var x = document.getElementById("more-about4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function myFunction11() {
+    var x = document.getElementById("more-about11");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function myFunction11() {
+    var x = document.getElementById("more-about11");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function myFunction12() {
+    var x = document.getElementById("more-about12");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
