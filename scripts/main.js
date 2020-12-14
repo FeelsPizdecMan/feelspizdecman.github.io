cb724e8a6231626083c26bb635a4552acab120e0
yan <58911833+FeelsPizdecMan@users.noreply.github.com>
2020-12-14 15:14:47 +0500

    d

--- scripts/main.js
+++ scripts/main.js
@@ -1,62 +1,76 @@
 function myFunction() {
     var x = document.getElementById("more-about");
-    if (x.style.display === "none") {
-      x.style.display = "block";
+    if (x.style.transform === "translateY(1500px)") {
+        x.style.transform = "translateY(0px)";
+        
     } else {
-      x.style.display = "none";
+      x.style.transform = "translateY(1500px)";
+      
     }
 }
 
 function myFunction2() {
     var x = document.getElementById("more-about2");
-    if (x.style.display === "none") {
-      x.style.display = "block";
+    if (x.style.transform === "translateY(1500px)") {
+        x.style.transform = "translateY(0px)";
+        
     } else {
-      x.style.display = "none";
+      x.style.transform = "translateY(1500px)";
+      
     }
 }
 
 function myFunction3() {
     var x = document.getElementById("more-about3");
-    if (x.style.display === "none") {
-      x.style.display = "block";
+    if (x.style.transform === "translateY(1500px)") {
+        x.style.transform = "translateY(0px)";
+        
     } else {
-      x.style.display = "none";
+      x.style.transform = "translateY(1500px)";
+      
     }
 }
 
 function myFunction4() {
     var x = document.getElementById("more-about4");
-    if (x.style.display === "none") {
-      x.style.display = "block";
+    if (x.style.transform === "translateY(1500px)") {
+        x.style.transform = "translateY(0px)";
+        
     } else {
-      x.style.display = "none";
+      x.style.transform = "translateY(1500px)";
+      
     }
 }
 
 function myFunction11() {
     var x = document.getElementById("more-about11");
-    if (x.style.display === "none") {
-      x.style.display = "block";
+    if (x.style.transform === "translateY(1500px)") {
+        x.style.transform = "translateY(0px)";
+        
     } else {
-      x.style.display = "none";
+      x.style.transform = "translateY(1500px)";
+      
     }
 }
 
 function myFunction11() {
     var x = document.getElementById("more-about11");
-    if (x.style.display === "none") {
-      x.style.display = "block";
+    if (x.style.transform === "translateY(1500px)") {
+        x.style.transform = "translateY(0px)";
+        
     } else {
-      x.style.display = "none";
+      x.style.transform = "translateY(1500px)";
+      
     }
 }
 
 function myFunction12() {
     var x = document.getElementById("more-about12");
-    if (x.style.display === "none") {
-      x.style.display = "block";
+    if (x.style.transform === "translateY(1500px)") {
+        x.style.transform = "translateY(0px)";
+        
     } else {
-      x.style.display = "none";
+      x.style.transform = "translateY(1500px)";
+      
     }
 }
