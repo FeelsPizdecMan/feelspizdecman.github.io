78e3b212d122a82fc4486f89f3f1a02e037248c5
yan <58911833+FeelsPizdecMan@users.noreply.github.com>
2020-12-16 21:31:34 +0500

    ddd

--- scripts/main.js
+++ scripts/main.js
@@ -74,3 +74,19 @@ function myFunction12() {
       
     }
 }
+
+
+// Fetch all the details element.
+const details = document.querySelectorAll("details");
+
+// Add the onclick listeners.
+details.forEach((targetDetail) => {
+  targetDetail.addEventListener("click", () => {
+    // Close all the details that are not targetDetail.
+    details.forEach((detail) => {
+      if (detail !== targetDetail) {
+        detail.removeAttribute("open");
+      }
+    });
+  });
+});
