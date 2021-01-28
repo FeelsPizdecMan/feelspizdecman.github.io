ebeb3b72781800ae4e841cb34467f976bc1aa1b1
yan <58911833+FeelsPizdecMan@users.noreply.github.com>
2021-01-28 19:22:44 +0500

    sadasd

--- code/main.js
+++ code/main.js
@@ -137,7 +137,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var primarySlider = new Splide('#primary-slider', {
       type: 'loop',
       heightRatio: 0.5,
-      width: "50vw",
+      width: "65vw",
       height: "75vh",
       pagination: false,
       arrows: false,
